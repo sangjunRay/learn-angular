5dc33c6d2c99825aeedb2e686a4da3abd37fbcb7
ray <raypark970421@gmail.com>
2022-05-10 16:31:56 +0900

    hero 속성 추가

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'searchAngular';
+  title = '앵귤러 이해';
   ray = 'ray'
 }
--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,15 +1,21 @@
+enum Gender {
+  M,F
+}
+
 export interface Hero {
   name: string;
   guild: string;
+  mbti: string;
+  gender: Gender;
 }
 
 export const HEROES = [
-  {name: 'Ray', guild:'Front-end'},
-  {name: 'Mila', guild:'Front-end'},
-  {name: 'Jerry', guild:'Front-end'},
-  {name: 'Sunny', guild:'Front-end'},
-  {name: 'Jhon', guild:'Back-end'},
-  {name: 'Woody', guild:'Back-end'},
-  {name: 'Martin', guild:'Front-end'},
-  {name: 'Jim', guild:'CTO'},
+  {name: 'Ray', guild:'Front-end', mbti: 'ENTP', gender: 0},
+  {name: 'Mila', guild:'Front-end', mbti: 'INTJ', gender: 1},
+  {name: 'Jerry', guild:'Front-end', mbti: 'ENTJ', gender: 0},
+  {name: 'Sunny', guild:'Front-end', mbti: 'INTJ', gender: 1},
+  {name: 'Jhon', guild:'Back-end', mbti: "INTJ", gender: 0},
+  {name: 'Woody', guild:'Back-end', mbti: 'ENTJ', gender: 0},
+  {name: 'Martin', guild:'Front-end', mbti: 'ISTP', gender: 0},
+  {name: 'Jim', guild:'CTO', mbti: 'INTP', gender: 0},
 ];
